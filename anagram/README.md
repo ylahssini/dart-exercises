20bf069a9e5f4dd6239b16bdb3977416e7b272b9
ylahssini <ylahssini@gmail.com>
2020-10-18 02:14:32 +0100

    add ansi pen & color the text

---
+++ anagram/README.md
@@ -0,0 +1,8 @@
+# Anagram
+
+This is the first exercise in this repo.
+After you install Dart on your computer, open the anagram folder and execute this command
+
+```dart anagram.dart```
+
+Enjoy!
---
+++ anagram/anagram.dart
@@ -0,0 +1,54 @@
+import 'dart:io';
+import 'package:ansicolor/ansicolor.dart';
+
+AnsiPen title = new AnsiPen()..rgb(r: 255, g: 229, b: 0);
+AnsiPen sub_title = new AnsiPen()..rgb(r: 1.0, g: 0.8, b: 0.2);
+AnsiPen success = new AnsiPen()..green(bold: true);
+AnsiPen error = new AnsiPen()..red(bold: true);
+
+String puncless(String str) {
+  var reg_cleaner = new RegExp(r'[ .,\/#!$%\^&\*;:{}=\-_`~()]');
+  return str.trim().replaceAll(reg_cleaner, '');
+}
+
+bool anagram(String str_a, String str_b) {
+  final String clean_str_a = puncless(str_a);
+  final String clean_str_b = puncless(str_b);
+  int similar_counter = 0;
+
+  for (var i = 0; i < clean_str_a.length; i++) {
+    if (clean_str_b.contains(clean_str_a[i])) {
+      similar_counter += 1;
+    }
+  }
+
+  return clean_str_a.length == similar_counter;
+}
+
+String type_string(String order) {
+  print('Type the $order string:');
+  String str = stdin.readLineSync();
+
+  if (puncless(str) == '') {
+    print(error('You must type the $order string (without space or punctuation)!'));
+    return type_string(order);
+  } else {
+    return str;
+  }
+}
+
+void main() {
+  print(title('-----------------------------   A N A G R A M   -----------------------------'));
+  print(sub_title('Check to see if two provided strings are anagrams of eachother.\n'));
+
+  String str_a = type_string('first');
+  String str_b = type_string('second');
+
+  if (anagram(str_a, str_b)) {
+    print(success('\nThe two words ($str_a, $str_b) are anagrams to eachother'));
+  } else {
+    print(error('\nNo, those two words ($str_a, $str_b) are not anagrams of eachother'));
+  }
+
+  print(title('-----------------------------   A N A G R A M   -----------------------------'));
+}
